c079363bd2bf66e903999720218602b6baf88a4e
Minmar03 <111729891+Minmar03@users.noreply.github.com>
2024-05-20 15:21:40 +0200

    levering c-krav
    siste sjekk før innlevering og oppdatering, funksjoner og styling

--- Frontend/src/components/API_SameType.jsx
+++ Frontend/src/components/API_SameType.jsx
@@ -1,5 +1,15 @@
 // API_SameType.jsx
 
+// Koden definerer en funksjon kalt fetchPokemonsWithSameType som er ansvarlig for å hente Pokémoner med samme type fra PokeAPI. 
+// Funksjonen tar imot typeName som parameter, som angir hvilken type Pokémoner som skal hentes.
+
+// Først gjør den en forespørsel til API-et for å hente data om den angitte typen ved å bruke typeName i URL-en.
+// Hvis responsen er vellykket, henter den dataene som JSON.
+// Deretter henter den detaljene for hver Pokémon i listen ved å gjøre en annen forespørsel for hver Pokémon-URL i listen.
+// For hver Pokémon hentes ID, navn, bilde-URL og type, og legges til i en ny liste.
+// Til slutt returnerer funksjonen den listen med Pokémon-objekter.
+// Hvis det oppstår en feil under henting av dataene, logges feilen til konsollen og kastes videre.
+
 const fetchPokemonsWithSameType = async (typeName) => {
     try {
         const response = await fetch(`https://pokeapi.co/api/v2/type/${typeName}`);
